b97344cb665e7b0b5200f8d6209143055085b5e6
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2023-08-11 17:58:44 +0100

    scalfolding

---
+++ messageWorker/Program.cs
@@ -0,0 +1,11 @@
+﻿using messageWorker;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
+
